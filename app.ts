cab72edd85e3eb1637544e6d540fb03b0d0cde1b
Tom <tgriley1@gmail.com>
2016-10-25 15:27:41 +0100

    Added GetCurrentAnimation

--- app.ts
+++ app.ts
@@ -1,58 +1,60 @@
-declare let canvasContext: CanvasRenderingContext2D;
+module Spryts {
+    export class SprytAnimation {
+        public Name: string;
+        public Row: number;
+        public FirstFrameIndex: number;
+        public LastFrameIndex: number;
 
-class SprytAnimation {
-    public Name: string;
-    public Row: number;
-    public FirstFrameIndex: number;
-    public LastFrameIndex: number;
-
-    constructor(name: string, row: number, firstFrameIndex: number, lastFrameIndex: number) {
-        this.Name = name;
-        this.Row = row;
-        this.FirstFrameIndex = firstFrameIndex;
-        this.LastFrameIndex = lastFrameIndex;
-    }
-}
-
-class Spryt {
-    private sprite: HTMLImageElement
-    private frameWidth: number;
-    private frameHeight: number;
-    private animations: { [name: string]: SprytAnimation; } = {};
-    private currentAnimation: SprytAnimation;
-    private animationFrameIndex: number = 0;
-
-    constructor(sprite: HTMLImageElement, frameWidth: number, frameHeight: number, defaultAnimation: SprytAnimation) {
-        this.sprite = sprite;
-        this.frameWidth = frameWidth;
-        this.frameHeight = frameHeight;
-        this.animations[defaultAnimation.Name] = defaultAnimation;
-        this.currentAnimation = this.animations[defaultAnimation.Name];
+        constructor(name: string, row: number, firstFrameIndex: number, lastFrameIndex: number) {
+            this.Name = name;
+            this.Row = row;
+            this.FirstFrameIndex = firstFrameIndex;
+            this.LastFrameIndex = lastFrameIndex;
+        }
     }
 
-    public Draw(x: number, y: number) {
-        if (this.animationFrameIndex > this.currentAnimation.LastFrameIndex) {
+    export class Spryt {
+        private sprite: HTMLImageElement
+        private frameWidth: number;
+        private frameHeight: number;
+        private animations: { [name: string]: SprytAnimation; } = {};
+        private currentAnimation: SprytAnimation;
+        private animationFrameIndex: number = 0;
+        private canvasContext: CanvasRenderingContext2D;
+
+        constructor(canvasContext: CanvasRenderingContext2D, sprite: HTMLImageElement, frameWidth: number, frameHeight: number, defaultAnimation: SprytAnimation) {
+            this.canvasContext = canvasContext;
+            this.sprite = sprite;
+            this.frameWidth = frameWidth;
+            this.frameHeight = frameHeight;
+            this.animations[defaultAnimation.Name] = defaultAnimation;
+            this.currentAnimation = this.animations[defaultAnimation.Name];
+        }
+
+        public Draw(x: number, y: number) {
+            if (this.animationFrameIndex > this.currentAnimation.LastFrameIndex) {
+                this.animationFrameIndex = 0;
+            }
+
+            let frameX = (this.animationFrameIndex) * this.frameWidth;
+            let frameY = (this.currentAnimation.Row) * this.frameHeight;
+            this.canvasContext.drawImage(this.sprite, frameX, frameY, this.frameWidth, this.frameHeight, x, y, this.frameWidth, this.frameHeight);
+
+            this.animationFrameIndex++;
+        }
+
+        public AddAnimation(sprytAnimation: SprytAnimation) {
+            this.animations[sprytAnimation.Name] = sprytAnimation;
+        }
+
+        public SetAnimation(name: string): void {
+            this.currentAnimation = this.animations[name];
             this.animationFrameIndex = 0;
         }
 
-        let frameX = (this.animationFrameIndex) * this.frameWidth;
-        let frameY = (this.currentAnimation.Row) * this.frameHeight;
-        canvasContext.drawImage(this.sprite, frameX, frameY, this.frameWidth, this.frameHeight, x, y, this.frameWidth, this.frameHeight);
-
-        this.animationFrameIndex++;
-    }
-
-    public AddAnimation(sprytAnimation: SprytAnimation) {
-        this.animations[sprytAnimation.Name] = sprytAnimation;
-    }
-
-    public SetAnimation(name: string) : void {
-        this.currentAnimation = this.animations[name];
-        this.animationFrameIndex = 0;
-    }
-
-    public GetCurrentAnimation(){
-        return this.currentAnimation.Name;
+        public GetCurrentAnimation() {
+            return this.currentAnimation.Name;
+        }
     }
 }
 
@@ -60,25 +62,29 @@ class Startup {
     public static main() {
         window.onload = () => {
             let canvas = <HTMLCanvasElement>document.getElementById('canvas');
-            canvasContext = canvas.getContext('2d');
+            let canvasContext = canvas.getContext('2d');
 
             let sprite = new Image();
             sprite.src = 'coin-sprite.png';
 
-            let animation1 = new SprytAnimation('animation1', 0, 0, 9)
-            let animation2 = new SprytAnimation('animation2', 1, 0 ,9)
+            let animation1 = new Spryts.SprytAnimation('animation1', 0, 0, 9)
+            let animation2 = new Spryts.SprytAnimation('animation2', 1, 0 ,9)
 
-            let spryt = new Spryt(sprite, 100, 100, animation1);
+            let spryt = new Spryts.Spryt(canvasContext, sprite, 100, 100, animation1);
             spryt.AddAnimation(animation2);
 
             spryt.SetAnimation('animation2');
 
             let ticks = 0
+
+            console.log(spryt.GetCurrentAnimation());
+
             window.setInterval(function () {
                 canvasContext.clearRect(0, 0, canvas.width, canvas.height);
                 
                 if(ticks === 9){
                     spryt.SetAnimation('animation1');
+                    console.log(spryt.GetCurrentAnimation());
                 }
                 
                 ticks++;
